04dc5f07881a57689d4d907cc162ad321002bcb3
Arbeits-Sachen <105115862+Arbeits-Sachen@users.noreply.github.com>
2022-11-07 18:58:48 +0000

    Update convert-to-old-roman.js

--- C-TDD/01-roman-numerals/convert-to-old-roman.js
+++ C-TDD/01-roman-numerals/convert-to-old-roman.js
@@ -1,5 +1,26 @@
-function convertToOldRoman(n) {
+function convertToOldRoman(n)
+{
+    let roman =
+    {
+        M: 1000,
+        D: 500,
+        C: 100,
+        L: 50,
+        X: 10,
+        V: 5,
+        I: 1
+    };
 
+    let str = "";
+    
+    for (let i of Object.keys(roman))
+    {
+        let q = Math.floor(n / roman[i]);
+        n -= q * roman[i];
+        str += i.repeat(q);
+    }
+
+    return str;
 }
 
 module.exports = convertToOldRoman;
