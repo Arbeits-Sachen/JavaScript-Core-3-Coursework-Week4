310605ccf17d2dd5dacf83c8ced777fef06de0f1
Arbeits-Sachen <105115862+Arbeits-Sachen@users.noreply.github.com>
2022-11-07 18:43:03 +0000

    Update convert-to-new-roman.js

--- C-TDD/01-roman-numerals/convert-to-new-roman.js
+++ C-TDD/01-roman-numerals/convert-to-new-roman.js
@@ -1,5 +1,32 @@
-function convertToNewRoman(n) {
+function convertToNewRoman(n)
+{
+    let roman =
+    {
+        M: 1000,
+        CM: 900,
+        D: 500,
+        CD: 400,
+        C: 100,
+        XC: 90,
+        L: 50,
+        XL: 40,
+        X: 10,
+        IX: 9,
+        V: 5,
+        IV: 4,
+        I: 1
+    };
 
+    let str = "";
+    
+    for (let i of Object.keys(roman))
+    {
+        let q = Math.floor(n / roman[i]);
+        n -= q * roman[i];
+        str += i.repeat(q);
+    }
+
+    return str;
 }
 
 module.exports = convertToNewRoman;
