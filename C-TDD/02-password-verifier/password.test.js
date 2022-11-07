313b5edc420b321575a739cbf684aef9fc1720d3
Arbeits-Sachen <105115862+Arbeits-Sachen@users.noreply.github.com>
2022-11-07 19:29:14 +0000

    Update password.test.js

--- C-TDD/02-password-verifier/password.test.js
+++ C-TDD/02-password-verifier/password.test.js
@@ -1,2 +1,23 @@
 let verify = require("./password");
 
+test("returns password rejected if password is less than 8 characters", function ()
+{
+    input = "1234";
+    expect(verify(input)).toEqual("Password rejected");
+});
+
+test("returns password rejected if password is null", function ()
+{
+    input = null;
+    expect(verify(input)).toEqual("Password rejected");
+});
+  
+test("returns password rejected if password doesn't include uppercase letters", function ()
+{
+    expect(verify("123456789a")).toEqual("Password rejected");
+});
+
+test("returns password rejected if password has no numbers", function ()
+{
+    expect(verify("asdfghjKl")).toEqual("Password rejected");
+});
