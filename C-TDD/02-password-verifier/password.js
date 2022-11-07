2b1b7d726610876d11c89db9d671ca8d4ce768f4
Arbeits-Sachen <105115862+Arbeits-Sachen@users.noreply.github.com>
2022-11-07 21:59:21 +0000

    Update password.js

--- C-TDD/02-password-verifier/password.js
+++ C-TDD/02-password-verifier/password.js
@@ -1,6 +1,6 @@
 function verify(password)
 {
-    if(password.length > 7 && password !== null && password.match(/[A-Z]/) && password.match(/[0-9]/))
+    if(password !== null && password.length > 7 && password.match(/[A-Z]/) && password.match(/[0-9]/))
     {
         return "Password accepted";
     }
